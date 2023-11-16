199c09dd471c134f0d8d02d5bfaee91c9d012e61
Long Lin <92683777+LongLinKU@users.noreply.github.com>
2023-11-16 10:16:22 +0100

    Create matrix.sh

---
+++ NJ-tree/matrix.sh
@@ -0,0 +1,1 @@
+/home/users/long/software/plink1.9/plink --bfile /davidData/data/malthe/gt/results/wildebeest/plink/Goat_wildebeest_variable_sites_mergehartebeest-topi_nomultiallelics_noindels_10dp_2het --distance square ibs allele-ct --out all.maf0.05.r2.0.99  --allow-extra-chr  --chr-set 29
